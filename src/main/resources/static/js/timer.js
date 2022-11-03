62cc13422a73f6f12af2485726d27b9205fcab25
Atul Bhatt <atulbhatt61@gmail.com>
2022-11-03 16:34:05 +0530

    Timer Working.

--- src/main/resources/static/js/timer.js
+++ src/main/resources/static/js/timer.js
@@ -1,1 +1,17 @@
-alert("this is working!");
+var start = Date.now();
+
+var si = setInterval(function() {
+    var delta = Date.now() - start; // milliseconds elapsed since start
+
+    output(Math.floor(delta / 1000)); // in seconds
+}, 1000); // update about every second
+
+timer = 20;
+function output(time){
+    document.getElementById("timer").innerHTML = timer;
+    if(timer < 0){
+        clearInterval(si);
+    }
+    timer--;
+    console.log(time);
+}
