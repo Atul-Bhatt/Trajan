dfea6c56ea3b4425baa32b24ff339d0602043f4b
Atul Bhatt <atulbhatt61@gmail.com>
2022-11-03 15:53:58 +0530

    JS working.

--- src/main/java/io/productivityapp/Trajan/controller/HomeApp.java
+++ src/main/java/io/productivityapp/Trajan/controller/HomeApp.java
@@ -1,13 +1,15 @@
 package io.productivityapp.Trajan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeApp {
 
     @GetMapping("/")
-    public String home(){
+    public String home(Model model) {
+        model.addAttribute("timerLength", 5);
         return "home";
     }
 }
