931b01459c05036265e479f9593cbc8b2a338e1d
Atul Bhatt <atulbhatt61@gmail.com>
2022-11-11 14:06:21 +0530

    Streak Model Created.

--- src/main/java/io/productivityapp/Trajan/model/StreakModel.java
+++ src/main/java/io/productivityapp/Trajan/model/StreakModel.java
@@ -1,4 +1,22 @@
 package io.productivityapp.Trajan.model;
 
 public class StreakModel {
+    private String task;
+    private int consecutiveDays;
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public int getConsecutiveDays() {
+        return consecutiveDays;
+    }
+
+    public void setConsecutiveDays(int consecutiveDays) {
+        this.consecutiveDays = consecutiveDays;
+    }
 }
