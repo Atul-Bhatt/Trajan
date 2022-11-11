1984ad1cc069d9f078090de9bcd4085918830bab
Atul Bhatt <atulbhatt61@gmail.com>
2022-11-11 14:04:15 +0530

    Task Model Created.

--- src/main/java/io/productivityapp/Trajan/model/TaskModel.java
+++ src/main/java/io/productivityapp/Trajan/model/TaskModel.java
@@ -1,4 +1,22 @@
 package io.productivityapp.Trajan.model;
 
 public class TaskModel {
+    private String task;
+    private int taskHours;
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public int getTaskHours() {
+        return taskHours;
+    }
+
+    public void setTaskHours(int taskHours) {
+        this.taskHours = taskHours;
+    }
 }
